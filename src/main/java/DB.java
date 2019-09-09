fc4a7bf99009c53925c4ce47dd6db9943a271c88
drewheathens <mudavadie@gmail.com>
2019-09-09 08:37:14 +0300

    java movies using mysql

---
+++ src/main/java/DB.java
@@ -0,0 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author evans
+ */
+
+    public class DB {
+
+    public static Connection mysql() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Moviedb", "evans", "evansMU#1");
+            
+            if (connection != null){
+                System.out.println("connected!!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection failure.");
+            return connection;
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        DB.mysql();
+
+        System.out.println("Testing application");
+        great.Url();
+        great.createTables();
+        great.insertMovies();
+        great.insertGenres();
+        great.insertMoviesGenres();
+//        great.insert();
+//        great.countGenre();
+    }
+
+}
+
+    
+
---
+++ src/test/java/DBTest.java
@@ -0,0 +1,60 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//import java.sql.Connection;
+//import java.sql.Statement;
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+///**
+// *
+// * @author evans
+// */
+//public class DBTest {
+//    
+//    public DBTest() {
+//        
+//    }
+//    
+//    Connection conn;
+//    Statement stmt;
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+////        conn = null;
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    /**
+//     * Test of postgresql method, of class DB.
+//     */
+//    @Test
+//    public void testPostgresql() {
+//        System.out.println("mysql");
+//        
+//        Connection result = DB.mysql();
+////        assertEquals(expResult, result);
+//
+//    }
+//
+//
+//    
+//}
